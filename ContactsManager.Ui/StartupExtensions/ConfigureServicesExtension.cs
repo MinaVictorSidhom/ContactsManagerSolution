36fd3ce48586f140b00c13429784f1564ac34d4b
Mina <minavictorsidhom@gmail.com>
2026-08-12 19:22:13 +0300

    Add Identity to IoC.

--- ContactsManager.Ui/StartupExtensions/ConfigureServicesExtension.cs
+++ ContactsManager.Ui/StartupExtensions/ConfigureServicesExtension.cs
@@ -1,5 +1,8 @@
-﻿using CRUDExample.Filters.ActionFilters;
+﻿using ContactsManager.Core.Domain.IdentityUser;
+using CRUDExample.Filters.ActionFilters;
 using Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Repositories;
 using RepositoryContracts;
@@ -42,6 +45,13 @@ namespace CRUDExample
                 });
 
             services.AddTransient<PersonsListActionFilter>();
+
+            //Enable Identity in this project 
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders()
+                .AddUserStore<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, Guid>>()
+                .AddRoleStore<RoleStore<ApplicationRole, ApplicationDbContext, Guid>>();
 
             services.AddHttpLogging(options => {
                 options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.
